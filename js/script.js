4fc41050cdf6ebb6beb6ff19059a14053732b870
Rocio <rocioalonso.dev@gmail.com>
2023-05-19 15:37:17 +0200

    v1.5

--- js/script.js
+++ js/script.js
@@ -4,9 +4,19 @@ window.onload = function () {
     const mobileMenuDropdown = document.getElementById("mobileMenuDropdown");
     const options = document.getElementById("options");
     
-    mobileMenuDropdown.addEventListener("click", () => {
-        toggleMobileMenu()
-    });
+    const icons = document.getElementById("icons");
+    const description = document.getElementById("description");
+    const features = document.getElementById("features");
+    const screens = document.getElementById("screens");
+
+
+
+    const linkDescription = document.getElementById("linkDescription");
+    const linkFeatures = document.getElementById("linkFeatures");
+    const linkScreens = document.getElementById("linkScreens");
+    
+    
+    mobileMenuDropdown.addEventListener("click", () => {toggleMobileMenu()});
 };
 
 window.onscroll = function() {scrollFunction()}
@@ -27,9 +37,25 @@ function scrollFunction() {
   } else {
     document.querySelector("nav").style.height="150px";
     document.querySelector("nav").style.boxShadow ="none";
-    
-
   }
+
+  const menuOptions = document.getElementsByClassName("menuOptions");
+  if(window.scrollY > (icons.offsetTop + icons.offsetHeight) && window.scrollY < (description.offsetTop + description.offsetHeight)){
+    linkFeatures.classList.remove('active')
+    linkScreens.classList.remove('active')
+    menuOptions[0].className += ' active';
+  }else if(window.scrollY > (description.offsetTop + description.offsetHeight) && window.scrollY < (features.offsetTop + features.offsetHeight)){
+    linkScreens.classList.remove('active')
+    linkDescription.classList.remove('active')
+    menuOptions[1].className += ' active';
+  }else if(window.scrollY > (features.offsetTop + features.offsetHeight) && window.scrollY < (screens.offsetTop + screens.offsetHeight)){
+    linkDescription.classList.remove('active')
+    linkFeatures.classList.remove('active')
+    menuOptions[2].className += ' active';
+  }else{
+    menuOptions.className.remove('active')
+  }
+
 }
 
 function resizeFunction(){
